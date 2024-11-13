f98207e709538583c2d6a5670db4cf7558f96547
avincer-coder <gar23078@byui.edu>
2024-11-13 14:37:02 -0600

    Miner editions

--- codewars_one.py
+++ codewars_one.py
@@ -27,24 +27,31 @@ c_third_number = float(input(f"Please enter your third number: "))
 product_c = c_first_number * c_second_number * c_third_number 
 
 # Función auxiliar para eliminar decimales si el número es entero
-def mostrar_sin_decimal(number):
+def show_without_decimal(number):
     return int(number) if number.is_integer() else number 
+# Para números sin parte decimal (ej. 5.0): Aunque el número es un float, su valor es un número entero (5.0). is_integer() devuelve True en este caso.
+
+# Para números con parte decimal (ej. 3.14): El número 3.14 no es un entero, pues tiene decimales .is_integer() devuelve False en este caso.
+
+#Usa .is_integer() cuando quieras verificar si un número float es efectivamente un número entero (sin decimales), y así decidir si mostrarlo como entero o flotante, como en el ejemplo anterior para formatear la salida.
 
 
-print(f"This 3 numbers form the first array assigned with letter c[{mostrar_sin_decimal(c_first_number)}, " f"{mostrar_sin_decimal(c_second_number)}, {mostrar_sin_decimal(c_third_number)}] \n" "Down below we will conform the array assigned to letter d.")
+
+
+print(f"This 3 numbers form the first array assigned with letter c[{show_without_decimal(c_first_number)}, " f"{show_without_decimal(c_second_number)}, {show_without_decimal(c_third_number)}] \n" "Down below we will conform the array assigned to letter d.")
 
 
 d_first_number = float(input(f"Please enter for d array your first number: "))
 d_second_number = float(input(f"Please enter your second number: "))
 d_third_number = float(input(f"Please enter your third number: "))
 
-print(f"The 3 numbers form the second array asinged with letter d[{d_first_number}, {d_second_number}, {d_third_number}]")
+print(f"The 3 numbers form the second array asinged with letter d are [{d_first_number}, {d_second_number}, {d_third_number}]")
 
 c_array = [c_first_number, c_second_number, c_third_number]
 b_array = [d_first_number, d_second_number, d_third_number]
 
 def second_difference(c,d):
     result = abs(prod(c)-prod(d))
-    print(f"The difference between your two arrays is: {result}")
+    print(f"The difference between your two arrays is: {show_without_decimal(result)}")
     
 second_difference(c_array, b_array)
