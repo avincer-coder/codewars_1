fd73ec530931c222262a94dee499c76ed2d4a161
avincer-coder <gar23078@byui.edu>
2024-11-13 14:27:02 -0600

    Small comment update

--- codewars_one.py
+++ codewars_one.py
@@ -1,4 +1,4 @@
-# abs, prod, .is_integer(), 
+# abs, prod imported from math, .is_integer()
 
 #------------------------Problem------------------------------- 
 # In this simple exercise, you will create a program that will take two lists of integers, a and b. Each list will consist of 3 positive integers above 0, representing the dimensions of cuboids a and b. You must find the difference of the cuboids' volumes regardless of which is bigger.
